b7caedf16f73651b1c28cc269415473506880a93
hit-thusz-RookieCJ <295951960@qq.com>
2021-08-24 10:12:11 +0800

    kernal

--- speech_enhance/audio_zen/model/module/attention_model.py
+++ speech_enhance/audio_zen/model/module/attention_model.py
@@ -37,3 +37,29 @@ class ChannelSELayer(nn.Module):
         a, b = squeeze_tensor.size()
         output_tensor = torch.mul(input_tensor, fc_out_2.view(a, b, 1))
         return output_tensor
+
+
+class ChannelECAlayer(nn.Module):
+    """Constructs a ECA module.
+    Args:
+        channel: Number of channels of the input feature map
+        k_size: Adaptive selection of kernel size
+    """
+
+    def __init__(self, channel, k_size=3):
+        super(ChannelECAlayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool1d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # feature descriptor on the global spatial information
+        y = self.avg_pool(x)
+
+        # Two different branches of ECA module
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+
+        # Multi-scale information fusion
+        y = self.sigmoid(y)
+
+        return x * y.expand_as(x)
--- speech_enhance/fullsubnet_plus/model/Amp_Attention_fullsubnet.py
+++ speech_enhance/fullsubnet_plus/model/Amp_Attention_fullsubnet.py
@@ -38,7 +38,10 @@ class Model(BaseModel):
         super().__init__()
         assert sequence_model in ("GRU", "LSTM"), f"{self.__class__.__name__} only support GRU and LSTM."
 
-        self.channel_attention = ChannelSELayer(num_channels=num_freqs//subband_num + 1)
+        if subband_num == 1:
+            self.channel_attention = ChannelSELayer(num_channels=num_freqs)
+        else:
+            self.channel_attention = ChannelSELayer(num_channels=num_freqs//subband_num + 1)
 
         self.fb_model = SequenceModel(
             input_size=num_freqs,
@@ -86,11 +89,13 @@ class Model(BaseModel):
         batch_size, num_channels, num_freqs, num_frames = noisy_mag.size()
         assert num_channels == 1, f"{self.__class__.__name__} takes the mag feature as inputs."
 
-        pad_num = self.subband_num - num_freqs % self.subband_num
-        # Fullband model
-        fb_input = functional.pad(self.norm(noisy_mag), [0, 0, 0, pad_num], mode="reflect")
-
-        fb_input = fb_input.reshape(batch_size, (num_freqs + pad_num) // self.subband_num, num_frames * self.subband_num)
+        if self.subband_num == 1:
+            fb_input = self.norm(noisy_mag).reshape(batch_size, num_channels * num_freqs, num_frames)
+        else:
+            pad_num = self.subband_num - num_freqs % self.subband_num
+            # Fullband model
+            fb_input = functional.pad(self.norm(noisy_mag), [0, 0, 0, pad_num], mode="reflect")
+            fb_input = fb_input.reshape(batch_size, (num_freqs + pad_num) // self.subband_num, num_frames * self.subband_num)
         fb_input = self.channel_attention(fb_input)
         fb_input = fb_input.reshape(batch_size, num_channels * (num_freqs + pad_num), num_frames)[:, :num_freqs, :]
         fb_output = self.fb_model(fb_input).reshape(batch_size, 1, num_freqs, num_frames)
