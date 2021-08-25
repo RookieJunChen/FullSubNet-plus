8d28cadae1d1d5c1c104af77e49d493f37d9c3f4
hit-thusz-RookieCJ <295951960@qq.com>
2021-08-25 14:52:05 +0800

    CBAM

--- speech_enhance/audio_zen/model/module/attention_model.py
+++ speech_enhance/audio_zen/model/module/attention_model.py
@@ -40,6 +40,45 @@ class ChannelSELayer(nn.Module):
         return output_tensor
 
 
+class ChannelCBAMLayer(nn.Module):
+    """
+    Re-implementation of Squeeze-and-Excitation (SE) block described in:
+        *Hu et al., Squeeze-and-Excitation Networks, arXiv:1709.01507*
+    """
+
+    def __init__(self, num_channels, reduction_ratio=2):
+        """
+        :param num_channels: No of input channels
+        :param reduction_ratio: By how much should the num_channels should be reduced
+        """
+        super(ChannelSELayer, self).__init__()
+        num_channels_reduced = num_channels // reduction_ratio
+        self.reduction_ratio = reduction_ratio
+        self.fc1 = nn.Linear(num_channels, num_channels_reduced, bias=True)
+        self.fc2 = nn.Linear(num_channels_reduced, num_channels, bias=True)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input_tensor):
+        """
+        :param input_tensor: X, shape = (batch_size, num_channels, H, W)
+        :return: output tensor
+        """
+        # batch_size, num_channels, T = input_tensor.size()
+        # Average pooling along each channel
+        mean_squeeze_tensor = input_tensor.mean(dim=2)
+        max_squeeze_tensor = input_tensor.max(dim=2)
+        # channel excitation
+        mean_fc_out_1 = self.relu(self.fc1(mean_squeeze_tensor))
+        max_fc_out_1 = self.relu(self.fc1(max_squeeze_tensor))
+        fc_out_1 = mean_fc_out_1 + max_fc_out_1
+        fc_out_2 = self.sigmoid(self.fc2(fc_out_1))
+
+        a, b = mean_squeeze_tensor.size()
+        output_tensor = torch.mul(input_tensor, fc_out_2.view(a, b, 1))
+        return output_tensor
+
+
 class ChannelECAlayer(nn.Module):
     """
      a ECA module.
