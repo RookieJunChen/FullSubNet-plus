ca73e8970c81ba9ec29504723bffc1e827dcec07
hit-thusz-RookieCJ <295951960@qq.com>
2021-08-26 09:20:33 +0800

    update att model

--- speech_enhance/fullsubnet_plus/model/Amp_Attention_fullsubnet.py
+++ speech_enhance/fullsubnet_plus/model/Amp_Attention_fullsubnet.py
@@ -8,9 +8,11 @@ from audio_zen.model.module.attention_model import ChannelSELayer, ChannelECAlay
 
 # for log
 from utils.logger import log
-print=log
 
-class Model(BaseModel):
+print = log
+
+
+class Full_Att_Model(BaseModel):
     def __init__(self,
                  num_freqs,
                  look_ahead,
@@ -42,7 +44,7 @@ class Model(BaseModel):
         if subband_num == 1:
             self.num_channels = num_freqs
         else:
-            self.num_channels = num_freqs//subband_num + 1
+            self.num_channels = num_freqs // subband_num + 1
 
         if channel_attention_model:
             if channel_attention_model == "SE":
@@ -101,24 +103,27 @@ class Model(BaseModel):
         assert num_channels == 1, f"{self.__class__.__name__} takes the mag feature as inputs."
 
         if self.subband_num == 1:
-            fb_input = self.norm(noisy_mag).reshape(batch_size, num_channels * num_freqs, num_frames)
+            fb_input = self.norm(noisy_mag).reshape(batch_size, num_channels * num_freqs, num_frames)   # [B, F, T]
             fb_input = self.channel_attention(fb_input)
         else:
             pad_num = self.subband_num - num_freqs % self.subband_num
             # Fullband model
             fb_input = functional.pad(self.norm(noisy_mag), [0, 0, 0, pad_num], mode="reflect")
-            fb_input = fb_input.reshape(batch_size, (num_freqs + pad_num) // self.subband_num, num_frames * self.subband_num)
+            fb_input = fb_input.reshape(batch_size, (num_freqs + pad_num) // self.subband_num,
+                                        num_frames * self.subband_num)  # [B, subband_num, T]
             fb_input = self.channel_attention(fb_input)
             fb_input = fb_input.reshape(batch_size, num_channels * (num_freqs + pad_num), num_frames)[:, :num_freqs, :]
         fb_output = self.fb_model(fb_input).reshape(batch_size, 1, num_freqs, num_frames)
 
         # Unfold the output of the fullband model, [B, N=F, C, F_f, T]
         fb_output_unfolded = self.unfold(fb_output, num_neighbor=self.fb_num_neighbors)
-        fb_output_unfolded = fb_output_unfolded.reshape(batch_size, num_freqs, self.fb_num_neighbors * 2 + 1, num_frames)
+        fb_output_unfolded = fb_output_unfolded.reshape(batch_size, num_freqs, self.fb_num_neighbors * 2 + 1,
+                                                        num_frames)
 
         # Unfold noisy input, [B, N=F, C, F_s, T]
         noisy_mag_unfolded = self.unfold(noisy_mag, num_neighbor=self.sb_num_neighbors)
-        noisy_mag_unfolded = noisy_mag_unfolded.reshape(batch_size, num_freqs, self.sb_num_neighbors * 2 + 1, num_frames)
+        noisy_mag_unfolded = noisy_mag_unfolded.reshape(batch_size, num_freqs, self.sb_num_neighbors * 2 + 1,
+                                                        num_frames)
 
         # Concatenation, [B, F, (F_s + F_f), T]
         sb_input = torch.cat([noisy_mag_unfolded, fb_output_unfolded], dim=2)
@@ -126,7 +131,146 @@ class Model(BaseModel):
 
         # Speeding up training without significant performance degradation. These will be updated to the paper later.
         if batch_size > 1:
-            sb_input = drop_band(sb_input.permute(0, 2, 1, 3), num_groups=self.num_groups_in_drop_band)  # [B, (F_s + F_f), F//num_groups, T]
+            sb_input = drop_band(sb_input.permute(0, 2, 1, 3),
+                                 num_groups=self.num_groups_in_drop_band)  # [B, (F_s + F_f), F//num_groups, T]
+            num_freqs = sb_input.shape[2]
+            sb_input = sb_input.permute(0, 2, 1, 3)  # [B, F//num_groups, (F_s + F_f), T]
+
+        sb_input = sb_input.reshape(
+            batch_size * num_freqs,
+            (self.sb_num_neighbors * 2 + 1) + (self.fb_num_neighbors * 2 + 1),
+            num_frames
+        )
+
+        # [B * F, (F_s + F_f), T] => [B * F, 2, T] => [B, F, 2, T]
+        sb_mask = self.sb_model(sb_input)
+        sb_mask = sb_mask.reshape(batch_size, num_freqs, 2, num_frames).permute(0, 2, 1, 3).contiguous()
+
+        output = sb_mask[:, :, :, self.look_ahead:]
+        return output
+
+
+class FullSub_Att_Model(BaseModel):
+    def __init__(self,
+                 num_freqs,
+                 look_ahead,
+                 sequence_model,
+                 fb_num_neighbors,
+                 sb_num_neighbors,
+                 fb_output_activate_function,
+                 sb_output_activate_function,
+                 fb_model_hidden_size,
+                 sb_model_hidden_size,
+                 channel_attention_model,
+                 norm_type="offline_laplace_norm",
+                 num_groups_in_drop_band=2,
+                 subband_num=10,
+                 weight_init=True,
+                 ):
+        """
+        FullSubNet model (cIRM mask)
+
+        Args:
+            num_freqs: Frequency dim of the input
+            sb_num_neighbors: Number of the neighbor frequencies in each side
+            look_ahead: Number of use of the future frames
+            sequence_model: Chose one sequence model as the basic model (GRU, LSTM)
+        """
+        super().__init__()
+        assert sequence_model in ("GRU", "LSTM"), f"{self.__class__.__name__} only support GRU and LSTM."
+
+        if subband_num == 1:
+            self.num_channels = num_freqs
+        else:
+            self.num_channels = num_freqs // subband_num + 1
+
+        if channel_attention_model:
+            if channel_attention_model == "SE":
+                self.channel_attention = ChannelSELayer(num_channels=self.num_channels)
+            elif channel_attention_model == "ECA":
+                self.channel_attention = ChannelECAlayer(channel=self.num_channels)
+            elif channel_attention_model == "CBAM":
+                self.channel_attention = ChannelCBAMLayer(num_channels=self.num_channels)
+            else:
+                raise NotImplementedError(f"Not implemented channel attention model {self.channel_attention}")
+
+        self.fb_model = SequenceModel(
+            input_size=num_freqs,
+            output_size=num_freqs,
+            hidden_size=fb_model_hidden_size,
+            num_layers=2,
+            bidirectional=False,
+            sequence_model=sequence_model,
+            output_activate_function=fb_output_activate_function
+        )
+
+        self.sb_model = SequenceModel(
+            input_size=(sb_num_neighbors * 2 + 1) + (fb_num_neighbors * 2 + 1),
+            output_size=2,
+            hidden_size=sb_model_hidden_size,
+            num_layers=2,
+            bidirectional=False,
+            sequence_model=sequence_model,
+            output_activate_function=sb_output_activate_function
+        )
+        self.subband_num = subband_num
+        self.sb_num_neighbors = sb_num_neighbors
+        self.fb_num_neighbors = fb_num_neighbors
+        self.look_ahead = look_ahead
+        self.norm = self.norm_wrapper(norm_type)
+        self.num_groups_in_drop_band = num_groups_in_drop_band
+
+        if weight_init:
+            self.apply(self.weight_init)
+
+    def forward(self, noisy_mag):
+        """
+        Args:
+            noisy_mag: noisy magnitude spectrogram
+
+        Returns:
+            The real part and imag part of the enhanced spectrogram
+
+        Shapes:
+            noisy_mag: [B, 1, F, T]
+            return: [B, 2, F, T]
+        """
+        assert noisy_mag.dim() == 4
+        noisy_mag = functional.pad(noisy_mag, [0, self.look_ahead])  # Pad the look ahead
+        batch_size, num_channels, num_freqs, num_frames = noisy_mag.size()
+        assert num_channels == 1, f"{self.__class__.__name__} takes the mag feature as inputs."
+
+        if self.subband_num == 1:
+            fb_input = self.norm(noisy_mag).reshape(batch_size, num_channels * num_freqs, num_frames)   # [B, F, T]
+            fb_input = self.channel_attention(fb_input)
+        else:
+            pad_num = self.subband_num - num_freqs % self.subband_num
+            # Fullband model
+            fb_input = functional.pad(self.norm(noisy_mag), [0, 0, 0, pad_num], mode="reflect")
+            fb_input = fb_input.reshape(batch_size, (num_freqs + pad_num) // self.subband_num,
+                                        num_frames * self.subband_num)  # [B, subband_num, T]
+            fb_input = self.channel_attention(fb_input)
+            fb_input = fb_input.reshape(batch_size, num_channels * (num_freqs + pad_num), num_frames)[:, :num_freqs, :]
+        fb_output = self.fb_model(fb_input).reshape(batch_size, 1, num_freqs, num_frames)
+
+        # Unfold the output of the fullband model, [B, N=F, C, F_f, T]
+        fb_output_unfolded = self.unfold(fb_output, num_neighbor=self.fb_num_neighbors)
+        fb_output_unfolded = fb_output_unfolded.reshape(batch_size, num_freqs, self.fb_num_neighbors * 2 + 1,
+                                                        num_frames)
+
+        # Unfold attention noisy input, [B, N=F, C, F_s, T]
+        noisy_mag_unfolded = self.unfold(fb_input, num_neighbor=self.sb_num_neighbors)
+        noisy_mag_unfolded = noisy_mag_unfolded.reshape(batch_size, num_freqs, self.sb_num_neighbors * 2 + 1,
+                                                        num_frames)
+
+        # Concatenation, [B, F, (F_s + F_f), T]
+        sb_input = torch.cat([noisy_mag_unfolded, fb_output_unfolded], dim=2)
+        sb_input = self.norm(sb_input)
+
+        # Speeding up training without significant performance degradation. These will be updated to the paper later.
+        if batch_size > 1:
+            sb_input = drop_band(sb_input.permute(0, 2, 1, 3),
+                                 num_groups=self.num_groups_in_drop_band)  # [B, (F_s + F_f), F//num_groups, T]
             num_freqs = sb_input.shape[2]
             sb_input = sb_input.permute(0, 2, 1, 3)  # [B, F//num_groups, (F_s + F_f), T]
 
@@ -148,7 +292,7 @@ if __name__ == "__main__":
     import datetime
 
     with torch.no_grad():
-        model = Model(
+        model = Full_Att_Model(
             sb_num_neighbors=15,
             fb_num_neighbors=0,
             num_freqs=257,
