287c375458f571fe211385224693556e45888173
hit-thusz-RookieCJ <295951960@qq.com>
2021-08-30 13:57:23 +0800

    update two stage large

--- speech_enhance/fullsubnet_plus/model/Two_Stage_FullSubNet.py
+++ speech_enhance/fullsubnet_plus/model/Two_Stage_FullSubNet.py
@@ -52,12 +52,14 @@ class Two_Stage_FullSubNet_Large(BaseModel):
             sb_model_hidden_size=sb_model_hidden_size,
             channel_attention_model=channel_attention_model,
             norm_type=norm_type,
-            num_groups_in_drop_band=num_groups_in_drop_band,
+            num_groups_in_drop_band=1,
             output_size=1,
             subband_num=subband_num,
             kersize=kersize,
             weight_init=weight_init
         )
+
+        self.middle_fc = nn.Linear(num_freqs * 2, num_freqs, bias=True)
 
         self.complex_model = Complex_FullSubNet(
             num_freqs=num_freqs,
@@ -69,7 +71,7 @@ class Two_Stage_FullSubNet_Large(BaseModel):
             sb_output_activate_function=sb_output_activate_function,
             fb_model_hidden_size=fb_model_hidden_size,
             sb_model_hidden_size=sb_model_hidden_size,
-            output_size=2,
+            output_size=1,
             norm_type=norm_type,
             num_groups_in_drop_band=num_groups_in_drop_band,
             weight_init=weight_init
@@ -96,13 +98,25 @@ class Two_Stage_FullSubNet_Large(BaseModel):
         noisy_mag, noisy_angle = mag_phase(noisy_complex)  # [B, F, T]
         noisy_mag = noisy_mag.unsqueeze(1)  # [B, 1, F, T]
         noisy_angle = noisy_angle.unsqueeze(1)  # [B, 1, F, T]
+        noisy_real = (noisy_complex.real).unsqueeze(1)  # [B, 1, F, T]
+        noisy_imag = (noisy_complex.imag).unsqueeze(1)  # [B, 1, F, T]
 
         IRM = self.amp_model(noisy_mag)  # IRM: [B, 1, F, T]
         enhanced_mag = IRM * noisy_mag  # [B, 1, F, T]
+
         enhanced_real = enhanced_mag * torch.cos(noisy_angle)
         enhanced_imag = enhanced_mag * torch.sin(noisy_angle)
+        real_con = torch.cat([enhanced_real, noisy_real], dim=2)  # [B, 1, 2 * F, T]
+        imag_con = torch.cat([enhanced_imag, noisy_imag], dim=2)  # [B, 1, 2 * F, T]
 
-        return enhanced_mag
+        # [B, 1, 2 * F, T] => [B, 1, T, 2 * F] => model => [B, 1, 2 * F, T]
+        real_input = (self.middle_fc(real_con.permute(0, 1, 3, 2))).permute(0, 1, 3, 2)
+        imag_input = (self.middle_fc(imag_con.permute(0, 1, 3, 2))).permute(0, 1, 3, 2)
+
+        complex_input = torch.cat([real_input, imag_input], dim=1)    # [B, 2, F, T]
+        cIRM = self.complex_model(complex_input)
+
+        return IRM, cIRM
 
 
 class Two_Stage_FullSubNet_Small(BaseModel):
