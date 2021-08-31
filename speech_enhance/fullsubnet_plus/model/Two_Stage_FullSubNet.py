c7401cae23e73bb403d4e904ed3a9691d4b7a58e
hit-thusz-RookieCJ <295951960@qq.com>
2021-08-31 10:49:14 +0800

    add residual

--- speech_enhance/fullsubnet_plus/model/Two_Stage_FullSubNet.py
+++ speech_enhance/fullsubnet_plus/model/Two_Stage_FullSubNet.py
@@ -448,3 +448,236 @@ class Two_Stage_AmpAttenion_without_fc_FullSubNet(BaseModel):
         cIRM = torch.cat([real_output, imag_output], dim=1)  # [B, 2, F, T]
 
         return IRM, cIRM
+
+
+class Two_Stage_Residual_AmpAttenion_FullSubNet(BaseModel):
+    def __init__(self,
+                 num_freqs,
+                 look_ahead,
+                 sequence_model,
+                 fb_num_neighbors,
+                 sb_num_neighbors,
+                 fb_output_activate_function,
+                 sb_output_activate_function,
+                 fb_model_hidden_size,
+                 sb_model_hidden_size,
+                 channel_attention_model="SE",
+                 norm_type="offline_laplace_norm",
+                 num_groups_in_drop_band=1,
+                 subband_num=1,
+                 kersize=[3, 5, 10],
+                 weight_init=True,
+                 ):
+        """
+        Two Stage model (cIRM mask)
+        """
+        super().__init__()
+        assert sequence_model in ("GRU", "LSTM"), f"{self.__class__.__name__} only support GRU and LSTM."
+
+        self.amp_model = FullBandNet(
+            num_freqs=num_freqs,
+            hidden_size=fb_model_hidden_size,
+            look_ahead=look_ahead,
+            sequence_model=sequence_model,
+            output_activate_function=fb_output_activate_function,
+            channel_attention_model=channel_attention_model,
+            norm_type=norm_type,
+            subband_num=subband_num,
+            kersize=kersize,
+            weight_init=weight_init
+        )
+
+        self.middle_fc = nn.Linear(num_freqs * 2, num_freqs, bias=True)
+
+        self.real_model = FullSub_Att_FullSubNet(
+            num_freqs=num_freqs,
+            look_ahead=look_ahead,
+            sequence_model=sequence_model,
+            fb_num_neighbors=fb_num_neighbors,
+            sb_num_neighbors=sb_num_neighbors,
+            fb_output_activate_function=fb_output_activate_function,
+            sb_output_activate_function=sb_output_activate_function,
+            fb_model_hidden_size=fb_model_hidden_size,
+            sb_model_hidden_size=sb_model_hidden_size,
+            channel_attention_model=channel_attention_model,
+            norm_type=norm_type,
+            num_groups_in_drop_band=num_groups_in_drop_band,
+            output_size=1,
+            subband_num=subband_num,
+            kersize=kersize,
+            weight_init=weight_init
+        )
+
+        self.imag_model = FullSub_Att_FullSubNet(
+            num_freqs=num_freqs,
+            look_ahead=look_ahead,
+            sequence_model=sequence_model,
+            fb_num_neighbors=fb_num_neighbors,
+            sb_num_neighbors=sb_num_neighbors,
+            fb_output_activate_function=fb_output_activate_function,
+            sb_output_activate_function=sb_output_activate_function,
+            fb_model_hidden_size=fb_model_hidden_size,
+            sb_model_hidden_size=sb_model_hidden_size,
+            channel_attention_model=channel_attention_model,
+            norm_type=norm_type,
+            num_groups_in_drop_band=num_groups_in_drop_band,
+            output_size=1,
+            subband_num=subband_num,
+            kersize=kersize,
+            weight_init=weight_init
+        )
+
+        self.num_groups_in_drop_band = num_groups_in_drop_band
+
+        if weight_init:
+            self.apply(self.weight_init)
+
+    def forward(self, noisy_complex):
+        """
+        Args:
+            noisy: noisy real part and imag part spectrogram
+
+        Returns:
+            The real part and imag part of the enhanced spectrogram
+
+        Shapes:
+            noisy_complex: [B, F, T]
+            return: [B, 2, F, T]
+        """
+        assert noisy_complex.dim() == 3
+        noisy_mag, noisy_angle = mag_phase(noisy_complex)  # [B, F, T]
+        noisy_mag = noisy_mag.unsqueeze(1)  # [B, 1, F, T]
+        noisy_angle = noisy_angle.unsqueeze(1)  # [B, 1, F, T]
+        noisy_real = (noisy_complex.real).unsqueeze(1)  # [B, 1, F, T]
+        noisy_imag = (noisy_complex.imag).unsqueeze(1)  # [B, 1, F, T]
+
+        IRM = self.amp_model(noisy_mag)  # IRM: [B, 1, F, T]
+        enhanced_mag = IRM * noisy_mag  # [B, 1, F, T]
+
+        enhanced_real = enhanced_mag * torch.cos(noisy_angle)
+        enhanced_imag = enhanced_mag * torch.sin(noisy_angle)
+        real_con = torch.cat([enhanced_real, noisy_real], dim=2)  # [B, 1, 2 * F, T]
+        imag_con = torch.cat([enhanced_imag, noisy_imag], dim=2)  # [B, 1, 2 * F, T]
+
+        # [B, 1, 2 * F, T] => [B, 1, T, 2 * F] => model => [B, 1, 2 * F, T]
+        real_input = (self.middle_fc(real_con.permute(0, 1, 3, 2))).permute(0, 1, 3, 2)
+        imag_input = (self.middle_fc(imag_con.permute(0, 1, 3, 2))).permute(0, 1, 3, 2)
+
+        real_output = self.real_model(real_input) + real_input
+        imag_output = self.imag_model(imag_input) + imag_input
+
+        output = torch.cat([real_output, imag_output], dim=1)  # [B, 2, F, T]
+
+        return IRM, output
+
+
+class Two_Stage_Residual_AmpAttenion_without_fc_FullSubNet(BaseModel):
+    def __init__(self,
+                 num_freqs,
+                 look_ahead,
+                 sequence_model,
+                 fb_num_neighbors,
+                 sb_num_neighbors,
+                 fb_output_activate_function,
+                 sb_output_activate_function,
+                 fb_model_hidden_size,
+                 sb_model_hidden_size,
+                 channel_attention_model="SE",
+                 norm_type="offline_laplace_norm",
+                 num_groups_in_drop_band=1,
+                 subband_num=1,
+                 kersize=[3, 5, 10],
+                 weight_init=True,
+                 ):
+        """
+        Two Stage model (cIRM mask)
+        """
+        super().__init__()
+        assert sequence_model in ("GRU", "LSTM"), f"{self.__class__.__name__} only support GRU and LSTM."
+
+        self.amp_model = FullBandNet(
+            num_freqs=num_freqs,
+            hidden_size=fb_model_hidden_size,
+            look_ahead=look_ahead,
+            sequence_model=sequence_model,
+            output_activate_function=fb_output_activate_function,
+            channel_attention_model=channel_attention_model,
+            norm_type=norm_type,
+            subband_num=subband_num,
+            kersize=kersize,
+            weight_init=weight_init
+        )
+
+        self.real_model = FullSub_Att_FullSubNet(
+            num_freqs=num_freqs,
+            look_ahead=look_ahead,
+            sequence_model=sequence_model,
+            fb_num_neighbors=fb_num_neighbors,
+            sb_num_neighbors=sb_num_neighbors,
+            fb_output_activate_function=fb_output_activate_function,
+            sb_output_activate_function=sb_output_activate_function,
+            fb_model_hidden_size=fb_model_hidden_size,
+            sb_model_hidden_size=sb_model_hidden_size,
+            channel_attention_model=channel_attention_model,
+            norm_type=norm_type,
+            num_groups_in_drop_band=num_groups_in_drop_band,
+            output_size=1,
+            subband_num=subband_num,
+            kersize=kersize,
+            weight_init=weight_init
+        )
+
+        self.imag_model = FullSub_Att_FullSubNet(
+            num_freqs=num_freqs,
+            look_ahead=look_ahead,
+            sequence_model=sequence_model,
+            fb_num_neighbors=fb_num_neighbors,
+            sb_num_neighbors=sb_num_neighbors,
+            fb_output_activate_function=fb_output_activate_function,
+            sb_output_activate_function=sb_output_activate_function,
+            fb_model_hidden_size=fb_model_hidden_size,
+            sb_model_hidden_size=sb_model_hidden_size,
+            channel_attention_model=channel_attention_model,
+            norm_type=norm_type,
+            num_groups_in_drop_band=num_groups_in_drop_band,
+            output_size=1,
+            subband_num=subband_num,
+            kersize=kersize,
+            weight_init=weight_init
+        )
+
+        self.num_groups_in_drop_band = num_groups_in_drop_band
+
+        if weight_init:
+            self.apply(self.weight_init)
+
+    def forward(self, noisy_complex):
+        """
+        Args:
+            noisy: noisy real part and imag part spectrogram
+
+        Returns:
+            The real part and imag part of the enhanced spectrogram
+
+        Shapes:
+            noisy_complex: [B, F, T]
+            return: [B, 2, F, T]
+        """
+        assert noisy_complex.dim() == 3
+        noisy_mag, noisy_angle = mag_phase(noisy_complex)  # [B, F, T]
+        noisy_mag = noisy_mag.unsqueeze(1)  # [B, 1, F, T]
+        noisy_angle = noisy_angle.unsqueeze(1)  # [B, 1, F, T]
+        noisy_real = (noisy_complex.real).unsqueeze(1)  # [B, 1, F, T]
+        noisy_imag = (noisy_complex.imag).unsqueeze(1)  # [B, 1, F, T]
+
+        IRM = self.amp_model(noisy_mag)  # IRM: [B, 1, F, T]
+        enhanced_mag = IRM * noisy_mag  # [B, 1, F, T]
+
+        enhanced_real = enhanced_mag * torch.cos(noisy_angle)
+        enhanced_imag = enhanced_mag * torch.sin(noisy_angle)
+
+        real_output = self.real_model(enhanced_real) + enhanced_real
+        imag_output = self.imag_model(enhanced_imag) + enhanced_imag
+        output = torch.cat([real_output, imag_output], dim=1)  # [B, 2, F, T]
+
+        return IRM, output
--- speech_enhance/fullsubnet_plus/trainer/trainer.py
+++ speech_enhance/fullsubnet_plus/trainer/trainer.py
@@ -12,7 +12,8 @@ plt.switch_backend('agg')
 
 
 class Trainer(BaseTrainer):
-    def __init__(self, dist, rank, config, resume, only_validation, model, loss_function, optimizer, train_dataloader, validation_dataloader):
+    def __init__(self, dist, rank, config, resume, only_validation, model, loss_function, optimizer, train_dataloader,
+                 validation_dataloader):
         super().__init__(dist, rank, config, resume, only_validation, model, loss_function, optimizer)
         self.train_dataloader = train_dataloader
         self.valid_dataloader = validation_dataloader
@@ -50,7 +51,8 @@ class Trainer(BaseTrainer):
                 RM, cRM = self.model(noisy_complex)
                 RM = RM.permute(0, 2, 3, 1)
                 cRM = cRM.permute(0, 2, 3, 1)
-                loss = self.alpha * self.loss_function(ground_truth_cIRM, cRM) + (1 - self.alpha) * self.loss_function(ground_truth_IRM, RM)
+                loss = self.alpha * self.loss_function(ground_truth_cIRM, cRM) + (1 - self.alpha) * self.loss_function(
+                    ground_truth_IRM, RM)
 
             self.scaler.scale(loss).backward()
             self.scaler.unscale_(self.optimizer)
@@ -117,6 +119,148 @@ class Trainer(BaseTrainer):
             enhanced_real = cRM[..., 0] * noisy_complex.real - cRM[..., 1] * noisy_complex.imag
             enhanced_imag = cRM[..., 1] * noisy_complex.real + cRM[..., 0] * noisy_complex.imag
             enhanced_complex = torch.stack((enhanced_real, enhanced_imag), dim=-1)
+            enhanced = self.torch_istft(enhanced_complex, length=noisy.size(-1))
+
+            noisy = noisy.detach().squeeze(0).cpu().numpy()
+            clean = clean.detach().squeeze(0).cpu().numpy()
+            enhanced = enhanced.detach().squeeze(0).cpu().numpy()
+
+            assert len(noisy) == len(clean) == len(enhanced)
+            loss_total += loss
+
+            # Separated loss
+            loss_list[speech_type] += loss
+            item_idx_list[speech_type] += 1
+
+            if item_idx_list[speech_type] <= visualization_n_samples:
+                self.spec_audio_visualization(noisy, enhanced, clean, name, epoch, mark=speech_type)
+
+            noisy_y_list[speech_type].append(noisy)
+            clean_y_list[speech_type].append(clean)
+            enhanced_y_list[speech_type].append(enhanced)
+
+            if self.rank == 0:
+                progress_bar.update(1)
+
+        log(f"[Test] Epoch {epoch}, Loss {loss_total / len(self.valid_dataloader)}")
+        self.writer.add_scalar(f"Loss/Validation_Total", loss_total / len(self.valid_dataloader), epoch)
+
+        for speech_type in ("With_reverb", "No_reverb"):
+            log(f"[Test] Epoch {epoch}, {speech_type}, Loss {loss_list[speech_type] / len(self.valid_dataloader)}")
+            self.writer.add_scalar(f"Loss/{speech_type}", loss_list[speech_type] / len(self.valid_dataloader), epoch)
+
+            validation_score_list[speech_type] = self.metrics_visualization(
+                noisy_y_list[speech_type], clean_y_list[speech_type], enhanced_y_list[speech_type],
+                visualization_metrics, epoch, visualization_num_workers, mark=speech_type
+            )
+
+        return validation_score_list["No_reverb"]
+
+
+class Residual_Trainer(BaseTrainer):
+    def __init__(self, dist, rank, config, resume, only_validation, model, loss_function, optimizer, train_dataloader,
+                 validation_dataloader):
+        super().__init__(dist, rank, config, resume, only_validation, model, loss_function, optimizer)
+        self.train_dataloader = train_dataloader
+        self.valid_dataloader = validation_dataloader
+
+    def _train_epoch(self, epoch):
+
+        loss_total = 0.0
+        progress_bar = None
+
+        if self.rank == 0:
+            progress_bar = tqdm(total=len(self.train_dataloader), desc=f"Training")
+
+        for noisy, clean in self.train_dataloader:
+            self.optimizer.zero_grad()
+
+            noisy = noisy.to(self.rank)
+            clean = clean.to(self.rank)
+
+            noisy_complex = self.torch_stft(noisy)
+            clean_complex = self.torch_stft(clean)
+
+            # noisy_input = torch.stack([noisy_complex.real, noisy_complex.imag], dim=1)
+            noisy_mag, _ = mag_phase(noisy_complex)
+            clean_mag, _ = mag_phase(clean_complex)
+
+            ground_truth_IRM = build_ideal_ratio_mask(noisy_mag, clean_mag)  # [B, F, T, 1]
+
+            # [B, 2, F, T] => [B, F, T, 2]
+            ground_truth_complex = torch.stack([clean_complex.real, clean_complex.imag], dim=1).permute(0, 2, 3, 1)
+
+            with autocast(enabled=self.use_amp):
+                # [B, F, T] => model => [B, 1, F, T], [B, 2, F, T] => [B, F, T, 1], [B, F, T, 2]
+                RM, enhanced_complex = self.model(noisy_complex)
+                RM = RM.permute(0, 2, 3, 1)
+                enhanced_complex = enhanced_complex.permute(0, 2, 3, 1)
+                loss = self.alpha * self.loss_function(ground_truth_complex, enhanced_complex) + (
+                            1 - self.alpha) * self.loss_function(
+                    ground_truth_IRM, RM)
+
+            self.scaler.scale(loss).backward()
+            self.scaler.unscale_(self.optimizer)
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.clip_grad_norm_value)
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+
+            loss_total += loss.item()
+
+            if self.rank == 0:
+                progress_bar.update(1)
+                progress_bar.refresh()
+
+        if self.rank == 0:
+            log(f"[Train] Epoch {epoch}, Loss {loss_total / len(self.train_dataloader)}")
+            self.writer.add_scalar(f"Loss/Train", loss_total / len(self.train_dataloader), epoch)
+
+    @torch.no_grad()
+    def _validation_epoch(self, epoch):
+        progress_bar = None
+        if self.rank == 0:
+            progress_bar = tqdm(total=len(self.valid_dataloader), desc=f"Validation")
+
+        visualization_n_samples = self.visualization_config["n_samples"]
+        visualization_num_workers = self.visualization_config["num_workers"]
+        visualization_metrics = self.visualization_config["metrics"]
+
+        loss_total = 0.0
+        loss_list = {"With_reverb": 0.0, "No_reverb": 0.0, }
+        item_idx_list = {"With_reverb": 0, "No_reverb": 0, }
+        noisy_y_list = {"With_reverb": [], "No_reverb": [], }
+        clean_y_list = {"With_reverb": [], "No_reverb": [], }
+        enhanced_y_list = {"With_reverb": [], "No_reverb": [], }
+        validation_score_list = {"With_reverb": 0.0, "No_reverb": 0.0}
+
+        # speech_type in ("with_reverb", "no_reverb")
+        for i, (noisy, clean, name, speech_type) in enumerate(self.valid_dataloader):
+            assert len(name) == 1, "The batch size for the validation stage must be one."
+            name = name[0]
+            speech_type = speech_type[0]
+
+            noisy = noisy.to(self.rank)
+            clean = clean.to(self.rank)
+
+            noisy_complex = self.torch_stft(noisy)
+            clean_complex = self.torch_stft(clean)
+
+            noisy_mag, _ = mag_phase(noisy_complex)
+            clean_mag, _ = mag_phase(clean_complex)
+
+            # noisy_input = torch.stack([noisy_complex.real, noisy_complex.imag], dim=1)
+
+            IRM = build_ideal_ratio_mask(noisy_mag, clean_mag)  # [B, F, T, 1]
+            # [B, 2, F, T] => [B, F, T, 2]
+            ground_truth_complex = torch.stack([clean_complex.real, clean_complex.imag], dim=1).permute(0, 2, 3, 1)
+
+            RM, enhanced_complex = self.model(noisy_complex)
+            RM = RM.permute(0, 2, 3, 1)
+            enhanced_complex = enhanced_complex.permute(0, 2, 3, 1)
+
+            loss = self.alpha * self.loss_function(ground_truth_complex, enhanced_complex) + (
+                        1 - self.alpha) * self.loss_function(IRM, RM)
+
             enhanced = self.torch_istft(enhanced_complex, length=noisy.size(-1))
 
             noisy = noisy.detach().squeeze(0).cpu().numpy()
