0a1bbfccd9f048078c6bd35aa74d9cc411cecd54
hit-thusz-RookieCJ <295951960@qq.com>
2021-09-19 09:20:17 +0800

    reshape2

--- speech_enhance/audio_zen/model/module/sequence_model.py
+++ speech_enhance/audio_zen/model/module/sequence_model.py
@@ -65,7 +65,7 @@ class SequenceModel(nn.Module):
                 TCNBlock(in_channels=input_size, hidden_channel=hidden_size, out_channels=input_size, dilation=1),
                 TCNBlock(in_channels=input_size, hidden_channel=hidden_size, out_channels=input_size, dilation=2),
                 TCNBlock(in_channels=input_size, hidden_channel=hidden_size, out_channels=input_size, dilation=5),
-                TCNBlock(in_channels=input_size, hidden_channel=hidden_size, out_channels=hidden_size, dilation=9),
+                TCNBlock(in_channels=input_size, hidden_channel=hidden_size, out_channels=input_size, dilation=9),
                 nn.ReLU()
             )
         else:
@@ -80,7 +80,7 @@ class SequenceModel(nn.Module):
         elif self.sequence_model_type == "TCN":
             self.fc_output_layer = nn.Linear(input_size, output_size)
         else:
-            self.fc_output_layer = nn.Linear(hidden_size, output_size)
+            self.fc_output_layer = nn.Linear(input_size, output_size)
 
         # Activation function layer
         if output_activate_function:
